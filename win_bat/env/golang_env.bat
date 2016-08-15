38c1a0aea66eff1944017191b5b7ebd567d14e07
test <example@example>
2016-08-15 19:57:55 +0900

    add call gorootenv.bat

--- win_bat/env/golang_env.bat
+++ win_bat/env/golang_env.bat
@@ -1,7 +1,9 @@
 
 @echo off
 
+if not "%DOTFILES_ROOT%" == "" goto :eof
 if not "%GOPATH%" == "" goto :eof
+call "%DOTFILES_ROOT%\win_bat\env\gorootenv.bat"
 
 echo --------------------
 echo seting GOPATH = %USERPROFILE%\golang
