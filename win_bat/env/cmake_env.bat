20d5ced7cd20facc006d46d069d17e936e58ec12
test <example@example>
2016-08-17 04:44:15 +0900

    chenge path

--- win_bat/env/cmake_env.bat
+++ win_bat/env/cmake_env.bat
@@ -8,7 +8,7 @@ if not "%SETUP_CMAKE%" == "" (
 
 echo setup_cmake
 
-set SETUP_CMAKE=%USERPROFILE%\opt\cmake-3.6.1-win64-x64\bin
+set SETUP_CMAKE=%USERPROFILE%\opt\cmake\latest\cmake\bin
 if not exist %SETUP_CMAKE% ( goto :eof )
 path=%PATH%;%SETUP_CMAKE%
 
