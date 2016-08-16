371b589d454472e0ea1660dd63860d5553669165
test <example@example>
2016-08-17 04:37:13 +0900

    fix comment

--- win_bat/pyvim32.bat
+++ win_bat/pyvim32.bat
@@ -8,7 +8,7 @@ echo define pyenv
 call "%WIN_ENVTOOLPATH%\python_env.bat"
 
 rem 必要になるまでコメントアウトしておく
-rem @call %USERPROFILE%\dotfiles\batch\set_Git_usr_bin.bat
+rem @call %USERPROFILE%\dotfiles\win_bat\env\git-cmd.bat
 rem
 
 call "gvim32"
