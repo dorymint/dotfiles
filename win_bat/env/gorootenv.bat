b6afefa1d23d065ce7c1b21c9e6c2be7c957a565
test <example@example>
2016-08-18 16:57:03 +0900

    fix

--- win_bat/env/gorootenv.bat
+++ win_bat/env/gorootenv.bat
@@ -4,8 +4,8 @@
 if not "%GOROOT%" == "" goto :eof
 
 echo --------------------
-echo seting GOROOT = %HOMEDRIVE%\Go\latest\go
+echo seting GOROOT = %HOMEDRIVE%\Go
 echo --------------------
 
-set GOROOT=%HOMEDRIVE%\Go\latest\go
+set GOROOT=%HOMEDRIVE%\Go
 PATH=%PATH%;%GOROOT%\bin
