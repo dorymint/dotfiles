34e5f551e183ac93422f370b65c2b492689b389e
dory <empty@empty>
2016-08-20 03:42:32 +0900

    shell scirpt fix

--- mklink.sh
+++ mklink.sh
@@ -6,13 +6,15 @@ export dotfiles_root=$(cd $(dirname $0); pwd)
 echo $dotfiles_root
 
 echo make symboliclink
+echo ""
 
-# use grml
-ln -s $dotfiles_root/zsh/zshrc.grml $HOME/.zshrc
-ln -s $dotfiles_root/zsh/zshrc.grml.local $HOME/.zshrc.local
+echo zsh
+ln -s $dotfiles_root/bash/zshrc $HOME/.zhsrc
 
+echo bash
 ln -s $dotfiles_root/bash/bashrc $HOME/.bashrc
 
+echo vim
 ln -s $dotfiles_root/vim/vimrc $HOME/.vimrc
 ln -s $dotfiles_root/vim/gvimrc $HOME/.gvimrc
 
