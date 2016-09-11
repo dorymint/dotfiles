81af4f15425edbce89ecae5962b1ee38755a584d
dory <empty@empty>
2016-09-11 19:49:12 +0900

    fix Changes to be committed: modified:   allcommit.sh

--- vim/sonicdir/allcommit.sh
+++ vim/sonicdir/allcommit.sh
@@ -8,14 +8,16 @@ if [[ -d "$DOTFILES_ROOT/vim/sonicdir" ]]; then
 
   # confirm
   key=""
-  while [[ -z $key ]] || [[ $key != "yes" ]]; do
-    echo "git commit [yes:no]?"
-    read key
-
-    if [[ $key = "no" ]] || [[ $key = "n" ]]; then
+  count=0
+  while [[ -z "$key" ]] || [[ "$key" != "yes" ]]; do
+    if [[ "$key" = "no" ]] || [[ "$key" = "n" ]] || [[ $count -gt 2 ]]; then
       echo "ok... stop process"
       exit 1
     fi
+    count=$(expr $count + 1)
+
+    echo "git commit [yes:no]?"
+    read key
   done
 
   git commit
