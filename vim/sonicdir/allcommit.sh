38eccc3a551c568763c0602a53e51ea29a522231
dory <empty@empty>
2016-09-11 18:37:08 +0900

    add confirm Changes to be committed: modified:   allcommit.sh

--- vim/sonicdir/allcommit.sh
+++ vim/sonicdir/allcommit.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-echo $DOTFILES_ROOT
-echo '$DOTFILES_ROOT'
-echo "$DOTFILES_ROOT"
-
 if [[ -d "$DOTFILES_ROOT/vim/sonicdir" ]]; then
   git add $DOTFILES_ROOT/vim/sonicdir/*
+  git status
+
+  # confirm
+  key=""
+  while [[ -z $key ]] || [[ $key != "yes" ]]; do
+    echo "git commit [yes:no]?"
+    read key
+
+    if [[ $key = "no" ]] || [[ $key = "n" ]]; then
+      echo "ok... stop process"
+      exit 1
+    fi
+  done
+
   git commit
 else
   echo "do not fnid template directory sonicdir"
