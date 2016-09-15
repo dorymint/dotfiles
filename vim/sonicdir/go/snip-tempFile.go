d9e8106de6706b770edf169857867a5b42c84ab4
dory <empty@empty>
2016-09-15 19:53:35 +0900

    add snip Changes to be committed: new file:   go/snip-tempFile.go

---
+++ vim/sonicdir/go/snip-tempFile.go
@@ -0,0 +1,19 @@
+
+// Create tempfile, Return filename.
+func tempFile(content string) (string, error) {
+	f, err := ioutil.TempFile("", "prefix")
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		if errclose := f.Close(); errclose != nil {
+			fmt.Fprintf(os.Stderr, "tempFile:%q\n", errclose)
+		}
+	}()
+	_, err = f.WriteString(content)
+	if err != nil {
+		return "", err
+	}
+	return f.Name(), nil
+}
+
