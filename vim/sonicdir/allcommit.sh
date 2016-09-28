6b5cc0f526b2ac43aab08524350befd923d8f6c0
dorymint <sk.dorymint@gmail.com>
2016-09-28 16:32:39 +0900

    test modified:   allcommit.sh

--- vim/sonicdir/allcommit.sh
+++ vim/sonicdir/allcommit.sh
@@ -7,20 +7,24 @@ if [[ -d "$DOTFILES_ROOT/vim/sonicdir" ]]; then
   exit
 
   # confirm
-  key=""
-  count=0
-  while [[ "$key" != "yes" ]] && [[ "$key" != "y" ]]; do
-    if [[ "$key" = "no" ]] || [[ "$key" = "n" ]] || [[ $count -gt 2 ]]; then
-      echo "ok... stop process"
-      exit 1
-    fi
-    count=$(expr $count + 1)
+  function confirm() {
+    key=""
+    count=0
+    while [[ "$key" != "yes" ]] && [[ "$key" != "y" ]]; do
+      if [[ "$key" = "no" ]] || [[ "$key" = "n" ]] || [[ $count -gt 2 ]]; then
+        echo "ok... stop process"
+        exit 1
+      fi
+      count=$(expr $count + 1)
 
-    echo "git commit [yes:no]?"
-    read key
-  done
+      echo "git commit [yes:no]?"
+      read key
+    done
+  }
 
-  git commit
+
+  git commit || exit 1
+  git status
 else
   echo "do not fnid template directory sonicdir"
 fi
