2efd5eb2310f47f8dca09f23e3b8fc970966c24d
dorymint <sk.dorymint@gmail.com>
2016-09-28 16:42:51 +0900

    add confirm for git push origin master Changes to be committed: modified:   allcommit.sh

--- vim/sonicdir/allcommit.sh
+++ vim/sonicdir/allcommit.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
+# confirm
+function confirm() {
+  local key=""
+  local count=0
+  while [[ "$key" != "yes" ]] && [[ "$key" != "y" ]]; do
+    if [[ "$key" = "no" ]] || [[ "$key" = "n" ]] || [[ $count -gt 2 ]]; then
+      echo "ok... stop process"
+      exit 1
+    fi
+    count=$(expr $count + 1)
+
+    echo $1
+    read key
+  done
+  return 0
+}
+
 if [[ -d "$DOTFILES_ROOT/vim/sonicdir" ]]; then
   cd "$DOTFILES_ROOT/vim/sonicdir" &&
   git add ./* &&
   git status ||
   exit
 
-  # confirm
-  function confirm() {
-    key=""
-    count=0
-    while [[ "$key" != "yes" ]] && [[ "$key" != "y" ]]; do
-      if [[ "$key" = "no" ]] || [[ "$key" = "n" ]] || [[ $count -gt 2 ]]; then
-        echo "ok... stop process"
-        exit 1
-      fi
-      count=$(expr $count + 1)
-
-      echo "git commit [yes:no]?"
-      read key
-    done
-  }
-
+  confirm 'git commit [yes:no]?'
 
   git commit || exit 1
   git status
+
+  confirm 'git push origin master [yes:no]?'
 else
   echo "do not fnid template directory sonicdir"
 fi
