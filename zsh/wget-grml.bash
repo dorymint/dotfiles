3218faa68454775cb29da8b34de6ae415d0a23ef
dorymint <sk.dorymint@gmail.com>
2017-02-08 12:22:23 +0900

    fix grml wget  Changes to be committed: 	new file:   .gitignore 	modified:   wget-grml.bash

--- zsh/wget-grml.bash
+++ zsh/wget-grml.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # wget -O is override
-wget -O .zshrc.grml http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
-wget -O .zshrc.grml.local http://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
+wget -O zshrc.grml.new http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+wget -O zshrc.grml.local.new http://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
 # REMEMBER!!: check the contents of rcs
 # EOF
