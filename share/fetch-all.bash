1db4d1e6b7b376e771f9838b8ddcdfdaf1cdfafa
dorymint <sk.dorymint@gmail.com>
2017-02-09 15:11:19 +0900

    add fetch scritps  Changes to be committed: 	new file:   share/fetch-all.bash 	modified:   vim/vimrc

---
+++ share/fetch-all.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+[ -z $1 ] && exit 1
+
+echo "fetch reps"
+
+for x in `cat $1`; do
+  [ -z "$x" ] && continue
+  echo "fetch ...$x"
+  cd "$(eval echo "$x")" && git fetch || continue
+  echo -e "done\n"
+done
+
+echo "all fetch done"
+# EOF
