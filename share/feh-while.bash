6ebb9a7bbd7e747ee65b413a563ed2311355f6b1
dorymint <sk.dorymint@gmail.com>
2017-02-09 17:24:01 +0900

    con  Changes to be committed: 	modified:   share/feh-while.bash 	modified:   termite/config

--- share/feh-while.bash
+++ share/feh-while.bash
@@ -6,6 +6,7 @@ wall="/usr/share/archlinux/wallpaper/$filetype
 while true; do
   feh --randomize --bg-scale --no-fehbg $wall
   echo -n "feh loop stop it?[stop:<C-c>]:"
+  sleep 2m
   read -t 10 key
   [[ "$key" == "stop" ]] && exit 0
   echo -en "\r"
