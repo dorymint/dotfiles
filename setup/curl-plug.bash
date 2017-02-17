cdf75dca9b9397c0a9692888250b0b6ff5bcd991
dorymint <sk.dorymint@gmail.com>
2017-02-17 15:07:36 +0900

    for plug  Changes to be committed: 	modified:   curl-plug.bash

--- setup/curl-plug.bash
+++ setup/curl-plug.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim.new --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
