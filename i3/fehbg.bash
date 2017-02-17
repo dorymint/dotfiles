a0bf61d22ba580d2c432c7c413624a4103b4703e
dorymint <sk.dorymint@gmail.com>
2017-02-17 15:00:14 +0900

    nyan  Changes to be committed: 	modified:   i3/config 	modified:   i3/fehbg.bash 	modified:   share/bin/feh-while.bash 	modified:   vim/vimrc

--- i3/fehbg.bash
+++ i3/fehbg.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 # for i3-wm
 dirs="/usr/share/backgrounds /usr/share/archlinux"
+if [[ -d "$HOME/Pictures/wall" ]];then
+  dirs="$dirs $HOME/Pictures/wall"
+fi
 feh --recursive --randomize --bg-scale $dirs
 # EOF
--- share/bin/feh-while.bash
+++ share/bin/feh-while.bash
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+set -e
+
 # TODO: reconsider filepath
 filetype='*[\(jpg\)\|\(png\)]'
 wall="/usr/share/archlinux/wallpaper/$filetype
 /usr/share/backgrounds/mate/*/$filetype
 /usr/share/backgrounds/xfce/$filetype"
+if [[ -d "$HOME/Pictures/wall" ]]; then
+  wall="$HOME/Pictures/wall/$filetype"
+fi
+
+echo "feh loop stop it?[<C-c>]:"
 while true; do
-  feh --randomize --bg-scale --no-fehbg $wall
-  echo -n "feh loop stop it?[stop:<C-c>]:"
-  sleep 2m
-  read -t 10 key
-  [[ "$key" == "stop" ]] && exit 0
-  echo -en "\r"
+  feh --randomize --bg-center --no-fehbg $wall || echo "feh: fallthrough"
+  sleep 1m
 done
 # EOF
