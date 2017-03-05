8b7e8b9031312133e747bb506a5823d2141f655d
dorymint <sk.dorymint@gmail.com>
2017-03-06 08:21:38 +0900

    fix  Changes to be committed: 	modified:   share/bin/ss10.bash 	modified:   x/xinitrc

--- share/bin/ss10.bash
+++ share/bin/ss10.bash
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -eu
-for x in `seq 100`; do ss -a && echo "$x" && sleep 3; done
+word=${1-:}
+if [ -z "$word" ];then
+  for x in `seq 100`; do ss -a && echo "$x" && sleep 10; done
+else
+  for x in `seq 100`; do
+    ss -a | grep "$word"
+    echo "$x"
+    sleep 10
+  done
+fi
 # EOF
