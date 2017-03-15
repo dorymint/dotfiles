fa9833ca07e394a17b243f3baffdb094eb9dc7af
dorymint <sk.dorymint@gmail.com>
2017-03-15 18:26:35 +0900

    fix mklink duplicated  Changes to be committed: 	modified:   mklink.sh

--- setup/mklink.sh
+++ setup/mklink.sh
@@ -1,48 +1,49 @@
-#!bin/bash
+#!bin/sh
 # scriptencoding utf-8
 
 echo $0
-dotfiles_root=$(cd $(dirname $0)/../; pwd -P )
+dotfiles_root=$(cd "$(dirname "$(readlink -f "$0")")"/../; pwd -P)
 echo "$dotfiles_root"
 
 echo ""
 echo "make symlink"
 echo ""
  
-[ -d $HOME/.vim ] || mkdir $HOME/.vim
-[ -d $HOME/.config ] || mkdir $HOME/.config
-[ -d $HOME/gowork ] || mkdir -p $HOME/gowork/{bin,pkg,src}
+[ -d "$HOME"/.vim ] || mkdir "$HOME"/.vim
+[ -d "$HOME"/.config ] || mkdir "$HOME"/.config
+[ -d "$HOME"/gowork ] || mkdir -p "$HOME"/gowork/{bin,pkg,src}
+#[ -d "$HOME"/go ] || mkdir -p "$HOME"/go/{bin,pkg,src}
 
 echo "zsh"
-ln -s $dotfiles_root/zsh/zshrc $HOME/.zshrc
+ln -sn "$dotfiles_root"/zsh/zshrc "$HOME"/.zshrc
 echo ""
 
 echo "bash"
-ln -s $dotfiles_root/bash/bashrc $HOME/.bashrc
+ln -sn "$dotfiles_root"/bash/bashrc "$HOME"/.bashrc
 echo ""
 
 echo "git"
-ln -s $dotfiles_root/gitconfig $HOME/.gitconfig
+ln -sn "$dotfiles_root"/gitconfig "$HOME"/.gitconfig
 echo ""
 
 echo "vim"
-ln -s $dotfiles_root/vim/vimrc $HOME/.vimrc
-ln -s $dotfiles_root/vim/gvimrc $HOME/.gvimrc
+ln -sn "$dotfiles_root"/vim/vimrc "$HOME"/.vimrc
+ln -sn "$dotfiles_root"/vim/gvimrc "$HOME"/.gvimrc
 echo ""
 
 echo "neovim"
-ln -s $HOME/.vim/ $HOME/.config/nvim
-ln -s $dotfiles_root/vim/vimrc $HOME/.vim/init.vim
+ln -sn "$HOME"/.vim/ "$HOME"/.config/nvim
+ln -sn "$dotfiles_root"/vim/vimrc "$HOME"/.vim/init.vim
 echo ""
 
 echo "tmux"
-ln -s $dotfiles_root/tmux/tmux.conf $HOME/.tmux.conf
+ln -sn "$dotfiles_root"/tmux/tmux.conf "$HOME"/.tmux.conf
 echo ""
 
 echo "i3-wm"
-ln -s $dotfiles_root/i3/ $HOME/.i3
+ln -sn "$dotfiles_root"/i3/ "$HOME"/.i3
 echo ""
 
 echo "termite"
-ln -s $dotfiles_root/termite/ $HOME/.config/
+ln -sn "$dotfiles_root"/termite/ "$HOME"/.config/
 # EOF
