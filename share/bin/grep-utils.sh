bfe880dd5ffa489ab9e7c3a74aeeebdc7343f6c1
dorymint <sk.dorymint@gmail.com>
2017-03-21 05:37:28 +0900

    modify grep option, -A 2 >> -A 3  Changes to be committed: 	modified:   share/bin/grep-utils.sh

--- share/bin/grep-utils.sh
+++ share/bin/grep-utils.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 utilsmd="$HOME"/github.com/"$USER"/hello-world/md/utils.md
-option="-A 2 -n --color=auto -e"
+option="-A 3 -n --color=auto -e"
 [ -r "$utilsmd" ]
 grep $option "${1}" "$utilsmd"
 # EOF
