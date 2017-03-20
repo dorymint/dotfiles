b1463b0421da48a41cc4256e126197c15d010b15
dorymint <sk.dorymint@gmail.com>
2017-03-21 05:10:36 +0900

    fix grep options  Changes to be committed: 	modified:   share/bin/grep-utils.sh

--- share/bin/grep-utils.sh
+++ share/bin/grep-utils.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 utilsmd="$HOME"/github.com/"$USER"/hello-world/md/utils.md
+option="-A 2 -n -e"
 [ -r "$utilsmd" ]
-grep -e "${1}" "$utilsmd"
+grep $option "${1}" "$utilsmd"
 # EOF
