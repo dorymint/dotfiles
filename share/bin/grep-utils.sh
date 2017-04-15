2137bd9ab676845877fd9867a7ff99638e45d7fa
dorymint <sk.dorymint@gmail.com>
2017-04-15 10:56:03 +0900

    update grep-utils.sh add flags  Changes to be committed: 	modified:   share/bin/grep-utils.sh

--- share/bin/grep-utils.sh
+++ share/bin/grep-utils.sh
@@ -3,7 +3,10 @@ set -eu
 
 # TODO: add flag for modify in grep-utils.path
 
+# TODO: reconsider, exchange $USER
 utilsmd="$HOME"/github.com/"$USER"/hello-world/md/utils.md
+# TODO: reconsider, bit confused, utilsmd utilspath
+# for reset utilsmd, see --local
 utilspath="$HOME"/dotfiles/etc/grep-utils.path
 context="-A 5"
 option="$context -n --color=auto -i -e"
@@ -19,6 +22,7 @@ helpmsg() {
   -f --file	path to target file (default $utilsmd)
   -l --local	use seved target file path (default $utilspath)
   -e --edit	edit target file, (default editor is vim)
+  -u --update	git add && commit for $utilsmd
   -B [N]	before context
   -A [N]	after context
   -C [N]	context
@@ -30,6 +34,11 @@ while [ -n "${1:-}" ]; do
     "-f"|"--file")shift; utilsmd="$1";;
     "-l"|"--local")utilsmd="$(cat "$utilspath")";;
     "-e"|"--edit")vim "$utilsmd"; exit 0;;
+    "-u"|"--update")
+      cd "$(dirname "$utilsmd")"
+      git add "$utilsmd"
+      git commit -m "up $(basename "$utilsmd")"
+      exit 0;;
     "-A")shift;context="-A $1";;
     "-B")shift;context="-B $1";;
     "-C")shift;context="-C $1";;
