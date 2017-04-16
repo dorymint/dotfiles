3d855b3a3dc48aff08c4654c8fe5754cc92f9ffa
dorymint <sk.dorymint@gmail.com>
2017-04-17 00:28:17 +0900

    update sonic

--- vim/sonicdir/allcommit.bash
+++ vim/sonicdir/allcommit.bash
@@ -12,7 +12,7 @@ function confirm() {
     fi
     count=$(expr $count + 1)
 
-    echo "$1"
+    echo -n "$1"
     read -t 60 key
   done
   return 0
@@ -21,7 +21,6 @@ function confirm() {
 if [[ -d "$DOTFILES_ROOT/vim/sonicdir" ]]; then
   cd "$DOTFILES_ROOT/vim/sonicdir"
   echo "git diff ."
-  sleep 1
   git diff .
   git status
   confirm 'git add . [yes:no]?'
@@ -29,13 +28,11 @@ if [[ -d "$DOTFILES_ROOT/vim/sonicdir" ]]; then
   git status
 
   echo "git diff --cached -- ."
-  sleep 1
   git diff --cached -- .
   confirm 'git commit -- . [yes:no]?'
   git commit -m "update sonic" -- .
 
   echo "git diff origin/master"
-  sleep 1
   git diff origin/master
   git status
   confirm 'git push origin master [yes:no]?'
