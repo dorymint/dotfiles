76dfcd2d53b4ecd150401a80812e657528c1c4e2
dorymint <sk.dorymint@gmail.com>
2017-04-25 13:14:40 +0900

    fix for x  Changes to be committed: 	modified:   setup/xln.bash 	modified:   termite/config 	modified:   vim/vimrc 	new file:   x/xresources

--- setup/xln.bash
+++ setup/xln.bash
@@ -1,4 +1,5 @@
-#!/bin/bash
-ln -s $HOME/dotfiles/x/xinitrc $HOME/.xinitrc
-ln -s $HOME/dotfiles/x/xserverrc $HOME/.xserverrc
+#!/bin/sh
+ln -s "$HOME"/dotfiles/x/xinitrc "$HOME"/.xinitrc
+ln -s "$HOME"/dotfiles/x/xserverrc "$HOME"/.xserverrc
+ln -s "$HOME"/dotifles/x/xresources "$HOME"/.Xresources
 # EOF
