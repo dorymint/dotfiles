01d111f7135a54e9191f09c3163e4676fd6bfca1
dorymint <sk.dorymint@gmail.com>
2017-04-25 13:18:12 +0900

    fix path  Changes to be committed: 	modified:   ../setup/xln.sh

--- setup/xln.sh
+++ setup/xln.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ln -s "$HOME"/dotfiles/x/xinitrc "$HOME"/.xinitrc
 ln -s "$HOME"/dotfiles/x/xserverrc "$HOME"/.xserverrc
-ln -s "$HOME"/dotifles/x/xresources "$HOME"/.Xresources
+ln -s "$HOME"/dotfiles/x/xresources "$HOME"/.Xresources
 # EOF
