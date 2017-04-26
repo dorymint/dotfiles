155b9ba57427b139361caef98376a0fb95d434ad
dorymint <sk.dorymint@gmail.com>
2017-04-26 15:02:25 +0900

    update sonic

--- vim/sonicdir/go/snip-mktemp.go
+++ vim/sonicdir/go/snip-mktemp.go
@@ -4,17 +4,11 @@ func mktemp(content string) (string, error) {
 	// use os temp directory
 	f, err := ioutil.TempFile("", "prefix")
 	if err != nil {
-		log.Printf("tmpFile:%v", err)
 		return "", err
 	}
-	defer func() {
-		if errclose := f.Close(); errclose != nil {
-			log.Printf("tempFile:%v", errclose)
-		}
-	}()
+	defer f.Close()
 	_, err = f.WriteString(content)
 	if err != nil {
-		log.Printf("tmpFile:%v", err)
 		return "", err
 	}
 	return f.Name(), nil
