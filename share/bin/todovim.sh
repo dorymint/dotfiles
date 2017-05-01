8a1e08f4aa591579282f11bc4a402c9204b23c19
dorymint <sk.dorymint@gmail.com>
2017-05-01 10:48:23 +0900

    nyan  Changes to be committed: 	modified:   todovim.sh

--- share/bin/todovim.sh
+++ share/bin/todovim.sh
@@ -5,7 +5,7 @@ set -eu
 [ -x "$(which peco)" ]
 
 word="TODO: "
-types="${2:-".go .txt"}"
+types=".go .txt"
 file=""
 root="./"
 
@@ -14,14 +14,17 @@ function helpmsg () {
   cat >&1 <<'END'
   todovim.sh
     vim "$(todogotcha | peco)"
-    $1=-root $2=-type
+    $1=-root
+    word --wrod -w
+    types --types -t
 END
 }
 while [ -n "${1:-}" ]; do
   case "$1" in
-   help|--help|-h) helpmsg; exit 0;;
+   help|--help|-h)helpmsg; exit 0;;
    word|--word|-w)shift; word="$1";;
-   *)shift; root="$1";;
+   types|--types|-t)shift; types="$1";;
+   *)root="$1";;
   esac
   shift
 done
