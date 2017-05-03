20d51c5298a5e817bae7cefac608be715cafff60
dorymint <sk.dorymint@gmail.com>
2017-05-03 12:36:18 +0900

    nyan  Changes to be committed: 	modified:   share/alias 	modified:   vim/sonicdir/go/snip-read.go

--- vim/sonicdir/go/snip-read.go
+++ vim/sonicdir/go/snip-read.go
@@ -3,7 +3,7 @@ func read(msg string) string {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
 	if sc.Err() != nil {
-		log.Fatalf("echo(): %v", sc.Err())
+		panic(sc.Err())
 	}
 	return sc.Text()
 }
