369975fff29dc0f8c3ac7b91db5a5b40059ecc27
kamisari <kamisari10yonaga@gmail.com>
2017-06-02 16:47:08 +0900

    sonic and add alias  Changes to be committed: 	modified:   share/alias 	modified:   vim/sonicdir/pretempl/go/snip-client-tcp.go

--- vim/sonicdir/pretempl/go/snip-client-tcp.go
+++ vim/sonicdir/pretempl/go/snip-client-tcp.go
@@ -1,14 +1,10 @@
-// Client main process, use tcp
-// example: Client(":8080")
-// retrun error = net.Dial
-func Client(prot string) error {
-	conn, err := net.Dial("tcp", prot)
+func client(port string) error {
+	conn, err := net.Dial("tcp", port)
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
 
-	// TODO: do something
-	// dosome()
+	// do something
 	return nil
 }
