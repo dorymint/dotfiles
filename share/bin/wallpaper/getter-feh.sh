072feb13fc2935eb61652245e4245c9ab339e685
kamisari <kamisari10yonaga@gmail.com>
2017-06-13 08:38:47 +0900

    add flag background  Changes to be committed: 	modified:   share/bin/wallpaper/getter-feh.sh

--- share/bin/wallpaper/getter-feh.sh
+++ share/bin/wallpaper/getter-feh.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 set -eu
+
+option="--scale-down"
+
+# help
+function helpmsg () {
+  cat >&1 <<END
+  -h --help help
+  -bg --background
+END
+}
+while [ -n "${1:-}" ]; do
+  case "$1" in
+   help|--help|-h) helpmsg; exit 0;;
+   -bg|--background) option="--bg-max";;
+  esac
+  shift
+done
+unset -f helpmsg
 [ -x "$(command which getter)" ]
 [ -x "$(command which feh)" ]
-feh --scale-down "$(getter)"
+feh $option "$(getter)"
 # EOF
