5c452544cff564e895ca5e83629ca199446c6940
kamisari <kamisari10yonaga@gmail.com>
2017-06-26 17:20:09 +0900

    rename variable walldir >> from  Changes to be committed: 	modified:   ln-wall.bash

--- share/bin/wallpaper/ln-wall.bash
+++ share/bin/wallpaper/ln-wall.bash
@@ -20,11 +20,11 @@ case "${1:-}" in
 esac
 unset -f helpmsg
 
-walldir=${1:-}
+from=${1:-}
 dst=${2:-}
-if [[ "$walldir" == "" ]] || [[ "$dst" == "" ]];then
+if [[ "$from" == "" ]] || [[ "$dst" == "" ]];then
   echo "invalid state"
-  echo 'require $1=walldir $2=dst'
+  echo 'require $1=from $2=dst'
   exit 1
 fi
 
@@ -32,12 +32,12 @@ dst=$(cd "$dst" && pwd)
 echo "ln-wall: dst=$dst"
 count=$(find "$dst" -type l | wc -l)
 
-walldir=$(cd "$walldir" && pwd)
-echo "ln-wall: walldir=$walldir"
-cd "$walldir"
+from=$(cd "$from" && pwd)
+echo "ln-wall: from=$from"
+cd "$from"
 for x in *; do
   if [ -f "$x" ]; then
-    ln -s "$walldir/$x" "$dst/$count" && count=$(expr $count + 1)
+    ln -s "$from/$x" "$dst/$count" && count=$(expr $count + 1)
   fi
 done
 # EOF
