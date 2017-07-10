c2ec031ab49de1237ea6f2cf953f1f7af6ef8649
kamisari <kamisari10yonaga@gmail.com>
2017-07-10 14:10:09 +0900

    fix msg  Changes to be committed: 	modified:   share/bin/ln/unlink-all.bash

--- share/bin/ln/unlink-all.bash
+++ share/bin/ln/unlink-all.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 cd "$1"
-echo "remove all symboliclinks in $(pwd) [yes:no]?"
+echo -n "remove all symboliclinks in $(pwd) [yes:no]?>"
 read -t 60 key
 [ "$key" == yes ]
 for x in *; do
