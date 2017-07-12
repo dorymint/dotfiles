22b95bf2ca1c5beedf9f4ceda037461fb341b4d3
kamisari <kamisari10yonaga@gmail.com>
2017-07-13 02:02:36 +0900

    fix env  Changes to be committed: 	modified:   share/rubyenv.sh

--- share/rubyenv.sh
+++ share/rubyenv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -d "$HOME"/.gem/ruby/"$(/usr/bin/ruby -e 'print Gem.user_dir')"/bin ]; then
-  export PATH="$HOME/.gem/ruby/"$(/usr/bin/ruby -e 'print Gem.user_dir')"/bin:$PATH"
+if [ -d "$(/usr/bin/ruby -e 'print Gem.user_dir')"/bin ]; then
+  export PATH=$(/usr/bin/ruby -e 'print Gem.user_dir')"/bin:$PATH"
 fi
 # EOF
