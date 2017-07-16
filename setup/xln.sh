33f8aa733c24ce8d6aa44221a5aae0474df5745d
kamisari <kamisari10yonaga@gmail.com>
2017-07-16 18:24:12 +0900

    fix for sway  Changes to be committed: 	modified:   setup/xln.sh 	modified:   share/alias 	modified:   x/sway/config

--- setup/xln.sh
+++ setup/xln.sh
@@ -7,8 +7,11 @@ set +e
 ln $lnopt "$HOME"/dotfiles/x/xinitrc "$HOME"/.xinitrc
 ln $lnopt "$HOME"/dotfiles/x/xserverrc "$HOME"/.xserverrc
 ln $lnopt "$HOME"/dotfiles/x/Xresources "$HOME"/.Xresources
-ln $lnopt "$HOME"/dotfiles/x/i3/ "$HOME"/.i3
-ln $lnopt "$HOME"/dotfiles/x/termite/ "$HOME"/.config/termite
 ln $lnopt "$HOME"/dotfiles/x/fontconfig/ "$HOME"/.config/fontconfig
+
+ln $lnopt "$HOME"/dotfiles/x/i3/ "$HOME"/.i3
+ln $lnopt "$HOME"/dotfiles/x/sway/ "$HOME"/.sway
+
+ln $lnopt "$HOME"/dotfiles/x/termite/ "$HOME"/.config/termite
 set -e
 # EOF
