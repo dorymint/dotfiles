8b374ef0aecfdcec2707ca1e0ac9a0ac42c06627
kamisari <kamisari10yonaga@gmail.com>
2017-07-17 12:12:22 +0900

    for sway  Changes to be committed: 	modified:   share/bin/x/wallpaper/getter-feh.sh

--- share/bin/x/wallpaper/getter-feh.sh
+++ share/bin/x/wallpaper/getter-feh.sh
@@ -1,24 +1,36 @@
 #!/bin/sh
 set -eu
 
+bgcmd="feh"
 option="--scale-down"
 
 # help
 function helpmsg () {
   cat >&1 <<END
+  use default feh
+
   -h --help help
   -bg --background
+
+  -sway --background-sway
 END
 }
 while [ -n "${1:-}" ]; do
   case "$1" in
    help|--help|-h) helpmsg; exit 0;;
    -bg|--background) option="--bg-max";;
+   -sway|--background-sway)
+     bgcmd='swaymsg -t command output "*" bg'
+     option="fit";;
   esac
   shift
 done
 unset -f helpmsg
 [ -x "$(command which getter)" ]
-[ -x "$(command which feh)" ]
-feh $option "$(getter)"
+if [ "$bgcmd" = "feh" ];then
+  [ -x "$(command which $feh)" ]
+  $bgcmd $option "$(getter)"
+else
+  $bgcmd "$(getter)" $option
+fi
 # EOF
