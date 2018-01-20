9476f0952da43e9fdba6cd89c2a819e325c7c068
kamisari <kamisari10yonaga@gmail.com>
2018-01-21 06:22:27 +0900

    up append

--- share/bin/append.sh
+++ share/bin/append.sh
@@ -1,31 +1,46 @@
 #!/bin/sh
 set -eu
 
-path="next.txt"
+list="next.list"
 
 # help
 helpmsg() {
   cat >&1 <<END
-  append.sh
+append.sh
+
+options:
+  -help
+  -exec
 END
 }
 
-main() {
-  if grep -w "${1}" "${path}"; then
+append() {
+  if grep -w "${1}" -- "${list}" &> /dev/null; then
     echo "duplicated: ${1}"
     exit 2
   fi
 
-  echo "${1}" >> "${path}"
+  echo "${1}" >> "${list}"
   echo "appended: ${1}"
   echo "cat:"
-  cat "${path}"
+  cat "${list}"
+}
+
+cmd() {
+  for x in "$(cat "${list}")"; do
+    ${*} -- ${x}
+    sleep $(shuf -i 20-180 -n 1)
+  done
+  # trunc
+  : > "${list}"
 }
 
 while [ -n "${1:-}" ]; do
-  case "$1" in
+  case "${1}" in
    help|-help|--help|-h) helpmsg; exit 0;;
-   *) main "${1}"; exit 0;;
+   # TODO: fix cmd
+   exec|-exec) cmd "echo" "-n"; exit 0;;
+   *) append "${1}"; exit 0;;
   esac
   shift
 done
