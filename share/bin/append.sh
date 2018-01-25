0755e50a3aed1ab28dd09f67b3fd25d9b222cd0b
kamisari <kamisari10yonaga@gmail.com>
2018-01-25 19:52:04 +0900

    nyan

--- share/bin/append.sh
+++ share/bin/append.sh
@@ -11,6 +11,7 @@ helpmsg() {
 	cat >&1 <<END
 append.sh
 	TODO: fix cmd name
+	append arguments to ${list}
 
 options:
 	-help
@@ -19,20 +20,33 @@ options:
 		action
 	-dry-run
 		check results for -exec
+	-xclip
+		append from xclip
 END
+}
+
+isvalid() {
+	if echo "${1:-}" | grep -e '^https\?://*' &> /dev/null; then
+		return 0
+	fi
+	return 1
 }
 
 # append to ${list}
 # ${1}=${string}
 append() {
+	if ! isvalid "${1}"; then
+		echo "invalid arguments: ${1}"
+		return 2
+	fi
 	if grep -w "${1}" -- "${list}" &> /dev/null; then
-		echo "duplicated: ${1}"
-		exit 2
+		echo "duplicated: ${1} in ${list}"
+		return 2
 	fi
 
 	echo "${1}" >> "${list}"
 	echo "appended: ${1}"
-	echo "cat:"
+	echo "--- cat ${list} ---"
 	cat "${list}"
 }
 
@@ -45,7 +59,7 @@ dryrun() {
 	done
 }
 
-cmd() {
+action() {
 	local l=$(cat "${list}")
 	for x in ${l}; do
 		${exe} "${x}"
@@ -59,11 +73,28 @@ cmd() {
 
 while [ -n "${1:-}" ]; do
 	case "${1}" in
-		help|-help|--help|-h) helpmsg; exit 0;;
-		exec|-exec) cmd; exit 0;;
-		dry-run|-dry-run) dryrun; exit 0;;
-		*) append "${1}"; exit 0;;
+		help|-help|--help|-h)
+			helpmsg
+			exit 0
+			;;
+		exec|-exec)
+			action
+			exit 0
+			;;
+		dry-run|-dry-run)
+			dryrun
+			exit 0
+			;;
+		xclip|-xclip)
+			append "$(xclip -o)"
+			exit 0
+			;;
+		*)
+			append "${1}"
+			exit 0
+			;;
 	esac
 	shift
 done
-unset -f helpmsg
+helpmsg
+exit 1
