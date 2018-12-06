33a33e83f33f45745a1b1a1a8e03031c9ddaf641
yaeshimo <hatsushimoyae@gmail.com>
2018-12-06 22:33:12 +0900

    rewrite

--- share/bin/grep-utils.sh
+++ share/bin/grep-utils.sh
@@ -1,118 +1,170 @@
 #!/bin/sh
 set -eu
 
-user="$(git config --global user.name)"
+name="grep-utils.sh"
+editor="${EDITOR:-"vim"}"
 
-utilsmd="$HOME"/github.com/"$user"/hello-world/md/utils.md
-# TODO: reconsider, bit confused utilsmd utilspath
-# for reset utilsmd, see --local
-utilspath="$HOME"/dotfiles/etc/grep-utils.path
-context="-A 5"
-color="--color=auto"
-option="$context -n $color -i -e"
-word=""
+# TODO: consider
+user="$(git config --global user.name)"
+file="$(readlink -e "$HOME"/github.com/"$user"/hello-world/md/utils.md)"
+
+# grep options
+options="--line-number --ignore-case --after-context=5"
+color="auto"
+
+helpmsg() {
+  cat >&1 <<END
+Usage:
+  $name [Options] -- PATTERN
+
+Options:
+  -h, --help        Display this message
+  -e, --edit        Open file by \$EDITOR (default $editor)
+  --color           Always output with color
+  --dir             Display parent directory
+
+  -d, --diff        Run git diff
+  -c, --commit      Run git commit
+  -p, --push-master Run git push origin master
+
+Examples:
+  $name --help
+
+END
+}
+
+errmsg() {
+  echo "${name}: $*" 1>&2
+}
+
+abort() {
+  errmsg "$*"
+  exit 2
+}
+
+ckargv() {
+  if [ $# -eq 0 ];then
+    return 0
+  else
+    abort "invalid arguments: $*"
+  fi
+}
+
+edit() {
+  $editor "$file"
+}
 
 # confirm $1=msg return bool
 confirm() {
-  local key=""
-  local counter=0
+  (
+  key=""
+  counter=0
   while [ $counter -lt 3 ]; do
-    counter=`expr $counter + 1`
+    counter=$(expr $counter + 1)
     echo -n "$1 [yes:no]?>"
-    read -t 60 key || return 1
+    read key || return 1
     case "$key" in
       no|n) return 1;;
       yes|y) return 0;;
     esac
   done
   return 1
+  )
 }
 
-# help
-unset -f helpmsg
-helpmsg() {
-  cat >&1 <<END
-grep-utils.sh [flags] ["grep word"]
-
-options:
-  -h -help --help
-    show help then exit
-  -f -file --file
-    path to target file (default $utilsmd)
-  -l -local --local
-    use seved target file path (default $utilspath)
-  -e -edit --edit
-    edit target file, (default editor is vim)
-  -c -commit --commit
-    git add && commit for $utilsmd
-  -p -push-master --push-master
-    git push origin master
-  -w -word --word
-    specify search word
-  -d -diff --diff
-    show diff $utilsmd
-  -B [N]
-    before context
-  -A [N]
-    after context
-  -C [N]
-    context
-  -color --color
-    always use color
-  -dir --dir
-    show target files directory
-END
+# for git
+gdiff() {
+  cd "$(dirname "$file")"
+  git diff "$(basename "$file")"
 }
-while [ -n "${1:-}" ]; do
-  case "$1" in
-    -h|-help|--help)helpmsg; exit 0;;
-    -f|-file|--file)shift; utilsmd="$1";;
-    -l|-local|--local)utilsmd="$(cat "$utilspath")";;
-    -e|-edit|--edit)vim "$utilsmd"; exit 0;;
-    -c|-commit|--commit)
-      cd "$(dirname "$utilsmd")"
-      git add "$utilsmd"
-      git commit -m "up $(basename "$utilsmd")" -- "$utilsmd"
+gcommit() {
+  (
+  base="$(basename "$file")"
+  msg="up $base"
+  cd "$(dirname "$file")"
+  git status
+  git diff -- "$base"
+  confirm "git add -- \"$base\" && git commit -m \"$msg\" -- \"$base\"" &&
+    git add -- "$base" &&
+    git commit -m "$msg" -- "$base" ||
+    abort "stopped"
+  )
+}
+gpush() {
+  cd "$(dirname "$file")"
+  git status
+  printf "git diff origin master <enter>"
+  read
+  git diff origin master
+  confirm "git push origin master" &&
+    git push origin master ||
+    abort "stopped"
+}
+
+main() {
+  grep $options --color="$color" --regexp="$*" -- "$file"
+}
+
+while true; do
+  case "${1:-}" in
+    # base
+    --)
+      shift
+      break
+      ;;
+    -h|--help|h|help|-help)
+      shift
+      helpmsg
+      ckargv "$@"
       exit 0
       ;;
-    -p|-push-master|--push-master)
-      cd "$(dirname "$utilsmd")"
-      git status
-      git diff origin master
-      confirm "run [git push origin master]"
-      git push origin master
-      exit 0 ;;
-    -w|-word|--word)shift; word="$1";;
-    -d|-dirr|--diff)
-      cd "$(dirname "$utilsmd")"
-      git diff "$(basename "$utilsmd")"
+    -e|--edit)
+      shift
+      ckargv "$@"
+      edit
       exit 0
       ;;
-    -A)shift;context="-A $1";;
-    -B)shift;context="-B $1";;
-    -C)shift;context="-C $1";;
-    -color|--color) color="--color=always";;
-    -dir|--dir)
-      # TODO: reconsider --dir
-      echo "$(dirname "$utilsmd")"
+    --color)
+      color="always"
+      ;;
+    --dir)
+      shift
+      ckargv "$@"
+      dirname "$file"
       exit 0
+      ;;
+
+    # git
+    -d|--diff)
+      shift
+      ckargv "$@"
+      gdiff
+      exit 0
+      ;;
+    -c|--commit)
+      shift
+      ckargv "$@"
+      gcommit
+      exit 0
+      ;;
+    -p|--push-master)
+      shift
+      ckargv "$@"
+      gpush
+      exit 0
+      ;;
+
+    # defaults
+    "")
+      errmsg "expected pattern but non"
+      exit 1
       ;;
     *)
-      if [ -n "$word" ]; then
-        echo "invalid argument: $*"
-        exit 1
-      fi
-      word="$1"
+      break
       ;;
   esac
   shift
 done
-unset -f helpmsg
-# update option
-option="$context -n $color -i -e"
 
-if [ ! -r "$utilsmd" ] || [ ! -f "$utilsmd" ]; then
-  echo "invalid filepath: $utilsmd"
-  exit 1
-fi
-grep $option "$word" "$utilsmd"
+main "$*"
+
