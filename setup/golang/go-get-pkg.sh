609f0d087eb0bae4848ca5a80bbf8f87b5625fff
yaeshimo <hatsushimoyae@gmail.com>
2018-12-18 21:21:30 +0900

    rewrite

--- setup/golang/go-get-pkg.sh
+++ setup/golang/go-get-pkg.sh
@@ -1,94 +1,98 @@
-#!/bin/bash
+#!/bin/sh
 set -eu
 
-# set variable
 options="-v"
 pkglist="$HOME/dotfiles/setup/golang/gopkg.list"
+upgrade=false
 
-split() {
-	echo "------- $1 -------"
+helpmsg() {
+	cat >&1 <<END
+Usage:
+  go-pkg-install.sh [Options]
+
+Optons:
+  -h, --help    Display this message
+  -u, --upgrade Append flag "-u" to "go get"
+  -f, --file    Specify path to gopkg.list (default: $pkglist)
+
+END
 }
 
 # confirm $1=msg return bool
 confirm() {
-	local key=""
-	local counter=0
-	while [ $counter -lt 3 ]; do
-		counter=`expr $counter + 1`
-		echo -n "$1 [yes:no]?>"
-		read -t 60 key || return 1
+	(
+	# which one?
+	msg="${1:-} [yes:no]?> "
+	#msg="${1:-}"
+	key=""
+	count=1
+	while true; do
+		[ $count -gt 3 ] && return 1
+		count=$(( $count + 1 ))
 		case "$key" in
-			no|n) return 1;;
-			yes|y) return 0;;
+			n|no) return 1;;
+			y|yes) return 0;;
 		esac
+		echo -n "$msg"
+		read key
 	done
-	return 1
+	echo "unreachable" >&2; exit 99
+	)
 }
 
-# help
-helpmsg() {
-	cat >&1 <<END
-gopkg-install.bash
-	go pkg install scripts
-
-optons:
-	help -help --help -h
-		show help message
-	update -update --update -u
-		add flag -u for go get
-	file -file --file -f
-		specify path to gopkg.list(default: $pkglist)
-END
+abort() {
+	echo "$*" >&2
+	exit 2
 }
-while [ -n "${1:-}" ]; do
+
+ckcom() {
+	command -v "$1" &> /dev/null || abort "command not found"
+}
+
+main() {
+	# depends
+	ckcom "gawk"
+	ckcom "go"
+
+	# parse
+	awkout="$(gawk '/^[^#].*/ { print $0 }' "$pkglist")"
+
+	go version
+	go env
+	echo "-- install list ---"
+	for x in $awkout; do
+		echo "$x"
+	done
+	echo "-------------------"
+
+	[ "$upgrade" = "true" ] && options="$options -u"
+
+	confirm "install packages? go get $options \$pkgs"
+	for x in $awkout; do
+		go get $options -- "${x}"
+	done
+}
+
+while [ $# -ne 0 ]; do
 	case "$1" in
-		help|-help|--help|-h)
-			helpmsg; exit 0
+		-h|--help|help|-help)
+			helpmsg
+			exit 0
 			;;
-		update|-update|--update|-u)
-			options="-v -u"
+		-u|--upgrade)
+			upgrade=true
 			;;
-		file|-file|--file|-f)
+		-f|--file)
 			shift
 			pkglist="$1"
 			;;
-		"")
-			# TODO: consider
+		*)
+			abort "invalid arguments: $*"
 			;;
 	esac
 	shift
 done
-unset -f helpmsg
 
-# check
-split "require"
-type go
-type gawk
-if [ ! -r "${pkglist}" ] || [ ! -f "${pkglist}" ]; then
-	echo "can't read ${pkglist}"
-	exit 1
-fi
+main
 
-# parse
-awkout=$(gawk '/^[^#].*/ { print $0 }' "$pkglist")
-
-# info && confirm
-split "go env"
-go version
-go env
-sleep 1
-split "install list"
-for x in ${awkout}; do
-	echo "${x}"
-done
-split "confirm"
-confirm "install packages? Run: go get ${options}"
-
-# install && update
-echo "Run: go get ${options} pkg"
-for x in ${awkout}; do
-	go get ${options} ${x}
-done
-echo "...finish"
-
-# vim: set noexpandtab shiftwidth=2 tabstop=2 softtabstop=2
+# vim: noexpandtab shiftwidth=2 tabstop=2 softtabstop=2
