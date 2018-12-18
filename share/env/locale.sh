55663b4217c1a8f6061e2902a48342b44314c2db
yaeshimo <hatsushimoyae@gmail.com>
2018-12-18 23:48:14 +0900

    fix profile

--- share/env/locale.sh
+++ share/env/locale.sh
@@ -8,3 +8,12 @@ else
   export LANG=en_US.UTF-8
   export LANGUAGE=en_US
 fi
+
+# XDG
+XDG_CONFIG_HOME="$HOME"/.config
+XDG_CACHE_HOME="$HOME"/.cache
+XDG_DATA_HOME="$HOME"/.local/share
+
+# set by pam_systemd
+#XDG_RUNTIME_DIR
+
